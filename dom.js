a3af7784abc3300406f9de1a86d8b1cb4efa547b
name <email>
2020-07-30 21:02:59 +0800

    dom添加与删除

--- dom.js
+++ dom.js
@@ -10,4 +10,13 @@ console.log('divLength', divClassName.length)
 console.log('div2', divClassName[1])
 
 let pTag = document.querySelectorAll('p')//集合
-console.log(pTag)
+let pTag1 = pTag[0]
+//porperty方式
+pTag1.style.color = 'red'
+pTag1.className = '第一个段落'
+//attribute方式
+pTag1.setAttribute('data-line', '5days')
+console.log(pTag1.getAttribute('data-line'))
+
+pTag1.setAttribute('style', 'color: blue')
+console.log(pTag1.getAttribute('style'))
